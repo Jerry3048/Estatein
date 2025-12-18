1df7313cb5b6ca068dd2482d00d5db1b2f13d6c3
Arewa Tolulope <tolulopearewa@student.oauife.edu.ng>
2025-12-18 05:00:28 +0100

    fix: Improve property details rendering and handle missing property case

--- src/Pages/PropertyDetails.tsx
+++ src/Pages/PropertyDetails.tsx
@@ -1,6 +1,6 @@
 import Navbar from "../Components/Navbar";
 import { useParams } from "react-router";
-import {FiMapPin} from "react-icons/fi";
+import { FiMapPin } from "react-icons/fi";
 import { usePropertyStore } from "../Store/usePropertyStore";
 
 const slugify = (text: string) =>
@@ -14,42 +14,35 @@ function PropertyDetails() {
     (p) => slugify(p.name) === name
   );
 
-  if (!property) {
-    return (
-      <p className="text-center text-white py-10">
-        Property not found
-      </p>
-    );
-  }
-
   return (
-   <div>
-    <Navbar />
-      <div className="mx-auto ">
-        {/* Left: Name + Location */}
+    <div>
+      <Navbar />
+
+      <div className="mx-auto">
+        {/* Name, Location & Price Section */}
         <div className="md:flex gap-3 items-center px-4 py-6 whitespace-nowrap">
-          <h1 className="text-2xl font-semibold mb-1 ">
-            {property.name}
+          <h1 className="text-2xl font-semibold mb-1">
+            {property?.name}
           </h1>
+
           <div className="flex justify-between md:items-center w-full mt-4 md:mt-0">
+            {/* Location */}
             <p className="text-sm border border-gray-600/30 rounded-sm px-2 py-1 inline-flex items-center gap-2">
               <FiMapPin />
-              {property.location}
+              {property?.location}
             </p>
-          
-  
-          {/* Right: Price */}
-          <div className="flex  md:flex-col items-center md:items-start">
-            <p className="text-xs text-gray-400">Price</p>
-            <p className="text-2xl font-semibold">
-              {property.price}
-            </p>
+
+            {/* Price */}
+            <div className="flex md:flex-col items-center md:items-start">
+              <p className="text-xs text-gray-400">Price</p>
+              <p className="text-2xl font-semibold">
+                {property?.price}
+              </p>
+            </div>
           </div>
         </div>
-        </div>
       </div>
-
-   </div>
+    </div>
   );
 }
 
